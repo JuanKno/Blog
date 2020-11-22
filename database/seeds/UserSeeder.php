84b07a53b489712b74e7d02a1afe41c69613b5c3
Juan Esteban Zapata <juanes0202@gmail.com>
2020-11-22 18:30:36 -0500

    wip

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -17,7 +17,7 @@ class UserSeeder extends Seeder
             'name' => 'Admin User',
             'password' => bcrypt('123456'),
             'email' => 'admin@admin.com',
-            'biography' => $faker->text(400),
+            'biography' =>'Alias necessitatibus sapiente molestias delectus in. Asperiores eum at magni est aliquam cupiditate reprehenderit totam. In hic omnis accusamus magnam sed. Perspiciatis alias libero maiores quis. Et eaque dolorem dolor eveniet exercitationem. Libero repellat consectetur ut labore molestiae.',
         ]);
 
         factory(User::class, 50)->create();
